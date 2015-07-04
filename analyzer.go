9d1d411979b45f42ddcd5a517621853cad70ee7a
Kien Nguyen <solidfoxrock@gmail.com>
2015-07-04 16:32:00 +0700

    Empty analyzer types

--- analyzer.go
+++ analyzer.go
@@ -3,3 +3,25 @@ package main
 type Analyzer interface {
 	Analyze(data []Commit) ([]Promotion, error)
 }
+
+type LanguageAnalyzer struct {
+}
+
+func (a *LanguageAnalyzer) Analyze(commits []Commit) ([]Promotion, error) {
+	return []Promotion{}, nil
+}
+
+type RulesAnalyzer struct {
+}
+
+func (a *RulesAnalyzer) Analyze(commits []Commit) ([]Promotion, error) {
+	return []Promotion{}, nil
+}
+
+func NewLanguageAnalyzer() Analyzer {
+	return &LanguageAnalyzer{}
+}
+
+func NewRulesAnalyzer() Analyzer {
+	return &RulesAnalyzer{}
+}
